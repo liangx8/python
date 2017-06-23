ea4a4be76d84c4a4f724a872fbfddb1cd6ef4d46
nameless.home <nameless@home.com>
2017-06-24 07:48:06 +0800

    none

--- xbin.py
+++ xbin.py
@@ -62,12 +62,6 @@ def vector_table(xx):
             if l[c-1] == '\n':
                 l=l[:c-1]
             li.append(l)
-        for x in core_table:
-            if x :
-                print(".weak {}_handler".format(x))
-        for x in li:
-            if x[0]!='0':
-                print('.weak {}_handler'.format(x))
         start=0x4
         for x in core_table:
             if x==None:
@@ -83,10 +77,17 @@ def vector_table(xx):
                 print(".long\t{}_handler\t\t/* {:2d} 0x0800 {:04x} */".format(x,idx,start))
             start = start + 4
             idx = idx +1
+        tmpl=".weak {}_handler\n.thumb_set {}_handler,Default_handler"
+        for x in core_table:
+            if x :
+                print(tmpl.format(x,x))
+        for x in li:
+            if x[0]!='0':
+                print(tmpl.format(x,x))
 
 if __name__=='__main__':
     vector_table('/home/arm/git/python/stm32f10xxx.txt')
 
 #    read("d:/lx/git/cpp/burn8051/x.bin",0xc00)
-#    read("d:/lx/git/motor/atmel/x.bin",0)
+#    read("/home/arm/Downloads/x.bin",0)
 #    readb("/home/arm/git/motor/c/eeprom.bin",0)
