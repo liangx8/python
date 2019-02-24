f1ad4c17bd91a1b67fddc252241899870696092b
chen.shen <chen.shen@office-computer.com>
2019-02-24 13:36:55 +0800

    define protocol

--- fileserver.py
+++ fileserver.py
@@ -1,13 +1,38 @@
 import socket
 import logging
 import sys
-import os.path
+import os
 CHAR_ENDIAN='little'
+
+
 
 def transfer(pipeio):
     """ 文件传输服务
-0 ~ 1 文件名长度(utf8编码)
-2 ~ n 文件名，根据第0~1字节决定"""
+command:
+  1 ls
+  2 upload
+  3 download
+  4 remove
+字符串定义
+   0 ~ 1 字符串长度(utf8编码)
+   2 ~ n 字符串内容，根据第0~1字节决定
+数值定义
+   4 bytes
+
+发送命令格式:
+  position 0: 命令
+  position 1: 命令的参数(字符串)，零长度的字符串意味无参数
+  position n: 只有upload命令需要这字段
+返回结果
+  成功
+    position 0: 0
+    position 1: rm, upload 无内容
+                ls, 返回指定的目录
+                download 指定文件的内容	
+  失败
+    position 0: 1
+    position 1: 失败原因(字符串)  
+"""
     try:
         bs=pipeio.recv(2)
         if len(bs) < 2:
@@ -34,10 +59,12 @@ if __name__== '__main__' :
         sock.close()
     else:
         logging.basicConfig(filename='copy.log',format='%(asctime)s %(levelname)s:%(message)s',level=logging.INFO)
-        if os.path.exists(SOCKFILE):
-            os.remove(SOCKFILE)
         sock = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
-        sock.bind(SOCKFILE)
+        try:
+            sock.bind(SOCKFILE)
+        except OSError as e:
+            os.remove(SOCKFILE)
+            sock.bind(SOCKFILE)
         sock.listen(1)
         while True:
             conn,addr = sock.accept()
