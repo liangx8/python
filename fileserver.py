2956c3e7b3f3353f0a85d6697e357b26825d9403
Mr Wang at home <wang.home.nobody@archcomputer.com>
2019-03-13 23:12:33 +0800

    snapshot

--- fileserver.py
+++ fileserver.py
@@ -4,7 +4,20 @@ import sys
 import os
 CHAR_ENDIAN='little'
 
-
+def readi32(reader):
+    bs = reader.recv(4)
+    if len(bs) < 4:
+        logging.error("expect 4 bytes but %d", len(bs))
+        os.exit(-1)
+    return int.from_bytes(bs,CHAR_ENDIAN)
+    
+def readStr(reader):
+    bs = reader.recv(2)
+    if len(bs) < 2:
+        logging.error("unknown incoming message %d", len(bs))
+        os.exit(-1)
+    fncnt=int.from_bytes(bs,CHAR_ENDIAN)
+    return reader.recv(fncnt).decode('utf-8')
 
 def transfer(pipeio):
     """ 文件传输服务
@@ -27,7 +40,7 @@ command:
   成功
     position 0: 0
     position 1: rm, upload 无内容
-                ls, 返回指定的目录
+                ls, 返回指定的目录"列文件实体格式"
                 download 指定文件的内容	
   失败
     position 0: 1
@@ -37,7 +50,7 @@ command:
   position 4: 文件名
 
 列文件实体格式
-  position 0: uint8 目录总数
+  position 0: uint16 目录总数
   position 1~n:文件实体
 
 """
@@ -67,9 +80,22 @@ def sendStr(io,s):
     
 SOCKFILE='/tmp/copy.sock'
 def returnDir(io):
-    pass
-    
-
+    stat=io.recv(1)
+    if stat[0] == 0:
+        stat=io.recv(2)
+        cnt=int.from_bytes(stat,CHAR_ENDIAN)
+        print("total: {}".format(cnt))
+        for idx in range(cnt):
+            size=readi32(io)
+            name=readStr(io)
+            print("{:8d} {}".format(size,name))
+    else:
+        print("server say:",readStr(io))
+# ls = 1
+# upload = 2
+# download = 3
+# rm = 4
+# quit = 5
 if __name__== '__main__' :
     argc=len(sys.argv)
     if argc > 1:
@@ -82,13 +108,22 @@ if __name__== '__main__' :
             else:
                 sendStr(sock,None)
             returnDir(sock)
+        elif sys.argv[1] == 'upload':
+            pass
+        elif sys.argv[1] == 'download':
+            print("downloading {}".format(argc))
+            if argc > 2:
+                sock.send(b'\3')
+                sendStr(sock,sys.argv[2])
+            else:
+                raise Exception('expected target of download')
+            
         elif sys.argv[1] == 'quit':
             sock.send(b'\5')
         while True:
             data=sock.recv(4096)
             if not data :break
-            print(data)
-        
+            print(data.decode('utf8'),end="")
         sock.close()
     else:
         logging.basicConfig(filename='copy.log',format='%(asctime)s %(levelname)s:%(message)s',level=logging.INFO)
