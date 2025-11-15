c940ed8c1ea074716e8667e7e3944474f973bce0
Mr. Copp <copper@desktop.home>
2025-11-15 11:10:00 +0800

    gas.py update

--- gas.py
+++ gas.py
@@ -1,6 +1,13 @@
 import datetime
-gasdate = (datetime.date(2024,12,18),datetime.date(2025,2,10),datetime.date(2025,4,1),datetime.date(2025,6,18),datetime.date(2025,9,16))
+gasdate = (datetime.datetime(2024,12,18),
+           datetime.datetime(2025,2,10),
+           datetime.datetime(2025,4,1),
+           datetime.datetime(2025,6,18),
+           datetime.datetime(2025,9,16),
+           datetime.datetime(2025,11,4,9,59,59))
 if __name__ == "__main__":
     cnt=len(gasdate)
     for x in range(cnt-1):
-        print(gasdate[x],"~",gasdate[x+1],"=>",gasdate[x+1]-gasdate[x])
+        a=gasdate[x]
+        n=datetime.datetime(a.year,a.month,a.day)
+        print(n,"~",gasdate[x+1],"=>",gasdate[x+1]-n)
